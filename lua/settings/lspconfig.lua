5a5ed1b445a2a1346deae4389224c9d76014a796
ArakiTakaki <arakitakaki.work@gmail.com>
2022-07-06 15:51:25 +0900

    delete: 不要なメソッドを除去

--- lua/settings/lspconfig.lua
+++ lua/settings/lspconfig.lua
@@ -2,7 +2,7 @@ if Util.isModuleAvailable('lspconfig') then
 	local nvim_lsp = require('lspconfig')
 	local on_attach = function(client, bufnr)
 		local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-		local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+		-- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 		local opts = { noremap=true, silent=true }
 		buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
 	end
