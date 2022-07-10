80c652810331ba7adf0a5caa37972e086088a3e6
ArakiTakaki <arakitakaki.work@gmail.com>
2022-07-10 12:33:42 +0900

    各種ファイルを変更

---
+++ lua/configs/env.lua
@@ -0,0 +1,5 @@
+local is = require 'is'
+
+if is.windows then
+    vim.g.python3_host_prog = 'C:\\develop\\python'
+end
--- lua/entry.lua
+++ lua/entry.lua
@@ -1,5 +1,7 @@
 vim.cmd[[packadd packer.nvim]]
+require('configs.env')
 require('configs.key')
+require('configs.indent')
 require('settings.packar')
 require('settings.lspconfig')
 require('settings.telescope')
